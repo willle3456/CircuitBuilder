753f9707cd6bbb8cecca74978bf5f4e2c95761c8
William Le <willle3456@gmail.com>
2018-01-20 23:31:05 -0800

    adding more test images and adjustments to the opencv code

--- test_bounding.py
+++ test_bounding.py
@@ -1,6 +1,7 @@
 import cv2
 #import numpy as np
 from matplotlib import pyplot as plt
+import collections
 
 #helpers for removing components that are not the ones we want
 def is_within(low, high, x):
@@ -20,38 +21,39 @@ def invert_image(img):
             img[i][j] = abs(255-img[i][j])
 
 #load image
-img = cv2.imread('test_images/Attach2.jpg', cv2.IMREAD_UNCHANGED)
-img = cv2.resize(img, (600,600))
-
+img = cv2.imread('test_images/test.jpg', cv2.IMREAD_UNCHANGED)
+img = cv2.resize(img, (1600, 900))
 #preprocessing
-blurred_img = cv2.GaussianBlur(cv2.cvtColor(img, cv2.COLOR_RGB2GRAY), (15,15), 0)
+kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (5,5))
+blurred_img = cv2.GaussianBlur(cv2.cvtColor(img, cv2.COLOR_RGB2GRAY), (13,13), 0)
 thres_img = cv2.adaptiveThreshold(blurred_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
         cv2.THRESH_BINARY, 11, 2)
 invert_image(thres_img)
-
+closed_img = cv2.morphologyEx(thres_img, cv2.MORPH_CLOSE, kernel)
+#canny_img = cv2.Canny(closed_img, 100, 200)
+cv2.imshow('test', closed_img)
 #contouring and bounding boxes
-img2, contours, hierarchy = cv2.findContours(thres_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+img2, contours, hierarchy = cv2.findContours(closed_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 imgs = []
 pts = []
+elements = {}
 for c in contours:
     x,y,w,h = cv2.boundingRect(c)
     tmp2 = [x,y,x+w, y+h]
-    if not is_inside(pts, tmp2):
+    if not is_inside(pts, tmp2) and w > 10 and h > 10:
         pts.append(tmp2)
-        tmp = img[y:y+h, x:x+w]
+        tmp = closed_img[y:y+h, x:x+w]
         imgs.append(tmp)
+        elements[x] = tmp
 cv2.namedWindow('test', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('image', 600, 600)
-
+#cv2.resizeWindow('image', 600, 600)
+ordered_elements = collections.OrderedDict(sorted(elements.items()))
 #resizing subimages for detection
 for i in range(len(imgs)):
     imgs[i] = cv2.resize(imgs[i], (28, 28), cv2.INTER_AREA)
-    imgs[i] = cv2.cvtColor(imgs[i], cv2.COLOR_RGB2GRAY)
-    r1, imgs[i] = cv2.threshold(imgs[i], 127, 255, cv2.THRESH_BINARY)
-    invert_image(imgs[i])
-    plt.subplot(3,4,i+1), plt.imshow(imgs[i], 'gray')
+    plt.subplot((len(imgs)/3 +1), 3,i+1), plt.imshow(imgs[i], 'gray')
 plt.show()
-
+print(ordered_elements)
 key = cv2.waitKey(0)
 if key == 27:
     cv2.destroyAllWindows()
